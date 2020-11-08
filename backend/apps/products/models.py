9194dfd312d4b441bf9e6550e3cb809bb3d5ac21
offstream <evangeline.niez@gmail.com>
2020-11-09 01:31:05 +0800

    Cleanup Product Model Meta Descriptions

--- backend/apps/products/models.py
+++ backend/apps/products/models.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.text import slugify
+from django.utils.translation import gettext_lazy as _
 
 # from apps.common.decorators import modify_model_fields
 from apps.common.models import Timestamps
@@ -24,25 +25,25 @@ def upload_location(instance, filename, **kwargs):
 
 # @modify_model_fields(created_at={"verbose_name": "Date Created"})
 class Product(Timestamps, models.Model):
-    name = models.CharField(max_length=150)
-    description = models.TextField()
-    image = models.ImageField(upload_to=upload_location, verbose_name="Photo")
+    name = models.CharField(_("name"), max_length=150)
+    description = models.TextField(_("description"))
+    image = models.ImageField(_("photo"), upload_to=upload_location)
     min_price = models.DecimalField(
-        default=0, max_digits=8, decimal_places=2, verbose_name="Minimum Bid Amount"
+        _("minimum bid amount"), default=0, max_digits=8, decimal_places=2
     )
     max_price = models.DecimalField(
-        default=0, max_digits=8, decimal_places=2, verbose_name="Maximum Bid Amount"
+        _("maximum bid amount"), default=0, max_digits=8, decimal_places=2
     )
     expiry_date = models.DateTimeField(
-        default=default_expirydate, verbose_name="Bid Submission Expiry Date"
+        _("bid closing date"), default=default_expirydate
     )
     owner = models.ForeignKey(
         get_user_model(), related_name="products", on_delete=models.CASCADE
     )
 
     class Meta:
-        verbose_name = "Product"
-        verbose_name_plural = "Products"
+        verbose_name = _("product")
+        verbose_name_plural = _("products")
 
     def __str__(self):
         return self.name
