12799dff012fb65e21392013e171b0fb699e0fbc
offstream <evangeline.niez@gmail.com>
2020-11-16 23:09:44 +0800

    Migrate Frontend Dev With Snowpack

---
+++ frontend/snowpack.config.js
@@ -0,0 +1,31 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/_dist_',
+  },
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-sass',
+    '@snowpack/plugin-typescript',
+  ],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    "/api": "http://localhost:8000/api"
+  },
+  alias: {
+    /* ... */
+  },
+};
